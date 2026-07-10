163f88783916c94d24c4ff75f9d9e8bee9989da7
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2026-07-10 19:52:43 +0300

    create: initialize project architecture

---
+++ src/pages/home-page/HomePage.tsx
@@ -0,0 +1,9 @@
+import styles from './HomePage.module.css';
+
+export const HomePage = () => {
+  return (
+    <main className={styles.page}>
+      <h1>Team Charlie Project</h1>
+    </main>
+  );
+}
