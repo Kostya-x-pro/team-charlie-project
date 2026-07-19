19a6907f3ec3b85cfdc1bcf1f8a7b830fe5eaa6e
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2026-07-19 20:12:14 +0300

    add shared text component

--- src/fsd-pages/home-page/HomePage.tsx
+++ src/fsd-pages/home-page/HomePage.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 import SnakeIcon from '@/shared/assets/icons/logo_small_icon.svg';
 import snakeImage from '@/shared/assets/images/hero_page_snake.png';
+import { Text } from '@/shared/ui/text';
 
 import styles from './HomePage.module.css';
 
@@ -13,6 +14,24 @@ export const HomePage = () => {
       {/* for images */}
       <Image src={snakeImage} alt='Snake' width={200} height={200} />
       <h1>Team Charlie Project</h1>
+
+      {/* for text */}
+      <Text tag='h1' size='20' weight='bold' lineHeight='24' color='white'>
+        We run an{' '}
+        <Text tag='span' size='20' weight='bold' color='yellow'>
+          in-house team
+        </Text>{' '}
+        of media buyers, designers, creatives, developers, and copywriters — no
+        middlemen, no outsourcing
+      </Text>
+
+      <Text family='stolzl' size='20'color='white' opacity='70'>
+        Custom tools, fast integrations and scalable architecture
+      </Text>
+
+      <Text tag='span' family='halvar' size='20' weight='bold' color='yellow' transform='uppercase' underline noWrap>
+        Team
+      </Text>
     </main>
   );
 };
