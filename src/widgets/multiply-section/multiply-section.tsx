08e37038942499cbe7bd56e906fbdec39a0ee720
Vlad <vladyslav.vovchenko.b@gmail.com>
2026-08-06 19:18:36 +0200

    footer layout

--- src/widgets/multiply-section/multiply-section.tsx
+++ src/widgets/multiply-section/multiply-section.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from 'react';
 
+import { Footer } from '@/widgets/footer';
+
 import ArrowDownIcon from '@/shared/assets/icons/Arrow_down_icon.svg';
 import { Button } from '@/shared/ui/button';
 import { Text } from '@/shared/ui/text';
@@ -15,68 +17,71 @@ export const MultiplySection = () => {
   const activeTab = MULTIPLY_ITEMS[activeTabId];
 
   return (
-    <section className={styles.section}>
-      <div className='container'>
-        <Text
-          className={styles.subtitle}
-          tag='div'
-          size='40'
-          weight='bold'
-          lineHeight='normal'
-          color='yellow'
-          align='right'
-          transform='uppercase'
-          letterSpacing='display-accent'
-          noWrap
-        >
-          MULTIPLY WITH US
-        </Text>
-        <div className={styles.wrapper}>
-          <div className={styles.tabs}>
-            {MULTIPLY_ITEMS.map(item => (
-              <Button
-                key={item.id}
-                variant='secondary'
-                active={item.id === activeTabId}
-                onClick={() => setActiveTabId(item.id)}
-              >
-                {item.label}
-              </Button>
-            ))}
-          </div>
-          <div className={styles.items}>
-            {activeTab && (
-              <div className={styles.item}>
-                <Text
-                  tag='p'
-                  family='halvar'
-                  size='20'
-                  weight='regular'
-                  lineHeight='24'
-                  color='white'
-                  align='center'
+    <div className={styles.section_wrapper}>
+      <section className={styles.section}>
+        <div className='container'>
+          <Text
+            className={styles.subtitle}
+            tag='div'
+            size='40'
+            weight='bold'
+            lineHeight='normal'
+            color='yellow'
+            align='right'
+            transform='uppercase'
+            letterSpacing='display-accent'
+            noWrap
+          >
+            MULTIPLY WITH US
+          </Text>
+          <div className={styles.wrapper}>
+            <div className={styles.tabs}>
+              {MULTIPLY_ITEMS.map(item => (
+                <Button
+                  key={item.id}
+                  variant='secondary'
+                  active={item.id === activeTabId}
+                  onClick={() => setActiveTabId(item.id)}
                 >
-                  {activeTab.firstText}
-                </Text>
-                <ArrowDownIcon className={styles.arrow_icon} />
-                <Text
-                  tag='p'
-                  family='halvar'
-                  size='20'
-                  weight='regular'
-                  lineHeight='24'
-                  color='white'
-                  align='center'
-                >
-                  {activeTab.secondText}
-                </Text>
-                <ArrowDownIcon className={styles.arrow_icon} />
-                <Button variant='primary'>{activeTab.buttonText}</Button>
-              </div>
-            )}
+                  {item.label}
+                </Button>
+              ))}
+            </div>
+            <div className={styles.items}>
+              {activeTab && (
+                <div className={styles.item}>
+                  <Text
+                    tag='p'
+                    family='halvar'
+                    size='20'
+                    weight='regular'
+                    lineHeight='24'
+                    color='white'
+                    align='center'
+                  >
+                    {activeTab.firstText}
+                  </Text>
+                  <ArrowDownIcon className={styles.arrow_icon} />
+                  <Text
+                    tag='p'
+                    family='halvar'
+                    size='20'
+                    weight='regular'
+                    lineHeight='24'
+                    color='white'
+                    align='center'
+                  >
+                    {activeTab.secondText}
+                  </Text>
+                  <ArrowDownIcon className={styles.arrow_icon} />
+                  <Button variant='primary'>{activeTab.buttonText}</Button>
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <Footer />
+    </div>
   );
 };
