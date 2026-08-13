010faa1ba24b102bb5e5f7131db8c0d659f9d07b
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2026-08-13 22:58:29 +0300

    update actual e2e test

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,13 +1,8 @@
 import { expect, test } from '@playwright/test';
 
 test('главная страница открывается', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/en');
 
-  await expect(page).toHaveTitle(/Team Charlie Project/);
-  await expect(
-    page.getByRole('heading', {
-      name: 'Team Charlie Project',
-      exact: true,
-    }),
-  ).toBeVisible();
+  await expect(page).toHaveTitle('Team Charlie Project');
+  await expect(page.getByRole('heading', { level: 1 })).toBeVisible();
 });
